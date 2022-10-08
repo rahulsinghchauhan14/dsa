7ac93b495815e5ecb53df498ec83b90fc5f8ba9c
rahulsinghchauhan14 <rahulsinghchauhan14@gmail.com>
2022-10-09 00:12:30 +0530

    patterns

--- patterns/all-star-patterns.cpp
+++ patterns/all-star-patterns.cpp
@@ -1,11 +1,360 @@
 #include<iostream>
 using namespace std;
 
+void pattern10(){
+/*
+         *
+        **
+       ***
+      ****
+     *****
+    ******
+   *******
+  ********
+ *********
+**********
+**********
+ *********
+  ********
+   *******
+    ******
+     *****
+      ****
+       ***
+        **
+         *
+*/
+
+int n =10;
+  int i=1,s,j;
+  
+   while (i <= n)
+   {	
+   		s=i; j=1;
+   		 while(s<n){
+        	cout << " ";
+            s++;
+        }
+        while(j <= i){
+        	cout << "*";
+            j++;
+        } 
+        cout << "\n";
+        i++;
+   }
+  i=0;
+  while (i < n)
+   {
+   		s=0; j=n;
+   		 while(s<i){
+        	cout << " "; s++;
+        }
+        while(j > i){
+        	cout << "*";
+            j--;
+        } 
+     
+        cout << "\n";
+        i++;
+   }
+}
+
+void pattern9(){
+/*
+
+         * * * * * * * * * * 
+        * * * * * * * * * * 
+       * * * * * * * * * * 
+      * * * * * * * * * * 
+     * * * * * * * * * * 
+    * * * * * * * * * * 
+   * * * * * * * * * * 
+  * * * * * * * * * * 
+ * * * * * * * * * * 
+* * * * * * * * * * 
+
+*/
+int n =10;
+  int i=1,s,j;
+  
+   while (i <= n)
+   {	
+   		s=i;
+        j=1;
+   		 while(s<n){
+        	cout << " ";
+            s++;
+        }
+        while(j <= n){
+        	cout << "* ";
+            j++;
+        } 
+        cout << "\n";
+        i++;
+   }
+}
+
+void pattern8(){
+/*
+
+*          
+* *         
+* * *        
+* * * *       
+* * * * *      
+* * * * * *     
+* * * * * * *    
+* * * * * * * *   
+* * * * * * * * *  
+* * * * * * * * * * 
+* * * * * * * * *  
+* * * * * * * *   
+* * * * * * *    
+* * * * * *     
+* * * * *      
+* * * *       
+* * *        
+* *         
+* 
+
+*/
+
+int n =10;
+  int i=1,s,j;
+  
+   while (i <= n)
+   {	
+   		s=i;
+        j=1;
+   		 
+        while(j <= i){
+        	cout << "* ";
+            j++;
+        } 
+     while(s<n){
+        	cout << " ";
+            s++;
+        }
+        cout << "\n";
+        i++;
+   }
+  i=1;
+  while (i < n)
+   {
+   		s=0; j=n;
+   		 
+        while(j > i){
+        	cout << "* ";
+            j--;
+        } 
+     while(s<i){
+        	cout << " "; s++;
+        }
+        cout << "\n";
+        i++;
+   }
+}
+
+void pattern7(){
+/*
+
+*          
+* *         
+* * *        
+* * * *       
+* * * * *      
+* * * * * *     
+* * * * * * *    
+* * * * * * * *   
+* * * * * * * * *  
+* * * * * * * * * * 
+* * * * * * * * * * 
+* * * * * * * * *  
+* * * * * * * *   
+* * * * * * *    
+* * * * * *     
+* * * * *      
+* * * *       
+* * *        
+* *         
+*
+
+*/
+
+int n =10;
+  int i=1,s,j;
+  
+   while (i <= n)
+   {	
+   		s=i;
+        j=1;
+   		 
+        while(j <= i){
+        	cout << "* ";
+            j++;
+        } 
+     while(s<n){
+        	cout << " ";
+            s++;
+        }
+        cout << "\n";
+        i++;
+   }
+  i=0;
+  while (i < n)
+   {
+   		s=0; j=n;
+   		 
+        while(j > i){
+        	cout << "* ";
+            j--;
+        } 
+     while(s<i){
+        	cout << " "; s++;
+        }
+        cout << "\n";
+        i++;
+   }
+}
+
+void pattern6(){
+    //Example 6: Program to print the full star diamond pattern in C.
+
+/*
+         * 
+        * * 
+       * * * 
+      * * * * 
+     * * * * * 
+    * * * * * * 
+   * * * * * * * 
+  * * * * * * * * 
+ * * * * * * * * * 
+* * * * * * * * * * 
+ * * * * * * * * * 
+  * * * * * * * * 
+   * * * * * * * 
+    * * * * * * 
+     * * * * * 
+      * * * * 
+       * * * 
+        * * 
+         * 
+*/
+
+int n =10;
+  int i=1,s,j;
+  
+   while (i <= n)
+   {	
+   		s=i;
+        j=1;
+   		 while(s<n){
+        	cout << " ";
+            s++;
+        }
+        while(j <= i){
+        	cout << "* ";
+            j++;
+        } 
+        cout << "\n";
+        i++;
+   }
+  i=1;
+  while (i < n)
+   {
+   		s=0; j=n;
+   		 while(s<i){
+        	cout << " "; s++;
+        }
+        while(j > i){
+        	cout << "* ";
+            j--;
+        } 
+        cout << "\n";
+        i++;
+   }
+}
+
+void pattern5(){
+// Example : Program in C to print the inverted half star pyramid pattern.
+
+/*
+*******************
+***************** 
+***************  
+*************   
+***********    
+*********     
+*******      
+*****       
+***        
+* 
+
+*/
+
+int n =10;
+  int i=n,s,j;
+  
+   while (i >=1)
+   {	
+   		s=i;
+        j=1;
+   		
+        while(j <= i*2-1){
+        	cout << "*";
+            j++;
+        }
+        while(s<n){
+        	cout << " ";
+            s++;
+        }
+        
+        cout << "\n";
+        i--;
+   }
+
+}
+
 void pattern4(){
-    
+/*
+* * * * * * * * * * * * * * * * * * * 
+ * * * * * * * * * * * * * * * * * 
+  * * * * * * * * * * * * * * * 
+   * * * * * * * * * * * * * 
+    * * * * * * * * * * * 
+     * * * * * * * * * 
+      * * * * * * * 
+       * * * * * 
+        * * * 
+         * 
+*/
+
+int n =10;
+  int i=n,s,j;
+  
+   while (i >= 1)
+   {	
+   		s=i;
+        j=1;
+   		while(s<n){
+        	cout << " ";
+            s++;
+        }
+        while(j <= i*2-1){
+        	cout << "* ";
+            j++;
+        }
+        
+        cout << "\n";
+        i--;
+   }
+
 }
 
 void pattern3(){
+// Example : Program in C to print inverted star pyramid pattern.
+
 /*
 *******************
  *****************
@@ -22,7 +371,7 @@ void pattern3(){
 int n =10;
   int i=n,s,j;
   
-   while (i!= 0 && i <= n)
+   while (i!= 0 && i <= n) // while (i >= 1)
    {	
    		s=i;
         j=1;
@@ -42,6 +391,8 @@ int n =10;
 }
 
 void pattern2(){
+// Example 3: Program in C to print half star pyramid pattern.
+
     /*
 *         
 ***        
@@ -77,6 +428,8 @@ void pattern2(){
 }
 
 void pattern1(){
+// Example 1: Program in C to print star pyramid pattern.
+
 /*
          *
         ***
