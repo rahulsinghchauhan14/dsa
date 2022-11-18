266f51995974196e7fdd92ffebcbd408219994dc
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2022-11-18 15:36:42 +0530

    Update 9-Palindrome Number.js

--- leetcode/9-Palindrome Number.js
+++ leetcode/9-Palindrome Number.js
@@ -26,4 +26,29 @@ var isPalindrome = function(x) {
     if(str === pal) return true
     else return false
  // runtime 153 ms
+     
+    
 };
+
+
+
+    // sample 129 ms submission
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    const num = x.toString().split("");
+    let reversedNums = [];
+    let isPalindrome = true; 
+    
+    for (let i = num.length - 1; i >= 0; i--) reversedNums.push(num[i]);
+    for(let i = 0; num.length > i; i++) {
+        if(num[i] !== reversedNums[i]) {
+             isPalindrome = false;
+             break;
+        }
+    }
+    return isPalindrome;
+};
+isPalindrome(5)
