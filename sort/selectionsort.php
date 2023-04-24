dd45204baac894f18c4d020e7cfbab3bb60e8e62
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2023-04-24 16:58:37 +0530

    selection sort

---
+++ sort/selectionsort.php
@@ -0,0 +1,25 @@
+<?php
+// selection sort
+
+/*
+select the minimum and swap the value
+*/
+
+$arr = [10,9,8,7,6,5,4,3,2,1]; 
+$n = count($arr);
+
+for($i=0; $i < $n-2; $i++){
+    $mini = $i;
+    for($j=$i; $j < $n; $j++){
+        if($arr[$i] > $arr[$j]){
+            $mini = $j;
+        }
+    }
+    $temp = $arr[$mini];
+    $arr[$mini] = $arr[$i];
+    $arr[$i] = $temp;
+}
+
+print_r($arr);
+
+?>
