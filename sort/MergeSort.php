b615fc55e7aaada9c70756d36c8b61aba1052e44
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2023-04-28 12:10:55 +0530

    Create MergeSort.php

---
+++ sort/MergeSort.php
@@ -0,0 +1,16 @@
+/*
+
+Merge Sort Algo
+TimeComplexity O(N log base 2 N)
+SpaceComplexity 
+- Divide & Merge
+- Recursion
+
+Algo 
+MergeShort ( arr, low, high ) 
+call recursively for
+MergeShort (arr, low, mid) - left array
+MergeShort (arr, mid+1, high) - right array
+Merge(arr,low, mid, high) - merge both the array
+
+*/
