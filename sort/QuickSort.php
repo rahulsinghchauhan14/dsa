57e3263c65bc8771d7a6debfa4cfbd0a76b04b7a
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2023-04-28 13:37:37 +0530

    Update QuickSort.php

--- sort/QuickSort.php
+++ sort/QuickSort.php
@@ -5,4 +5,16 @@ after tweaks DESC order
 Better than MergeSort
 TimeComplexity - O(N log N)
 SpaceComplexity - O(1) // no temp array is used
+
+[
+a. 1st element in the array
+b. last element in the array
+c. median of the array
+d. random element of the array
+]
+
+1. Pick a pivot & place it in its correct place in the sorted array
+ 
+2. Smaller on the left and larger on the right
+
 */
