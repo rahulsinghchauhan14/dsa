b2955ab4722e44acb6fd33d197c7841db10aa91c
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2023-04-28 15:00:08 +0530

    Update QuickSort.php

--- sort/QuickSort.php
+++ sort/QuickSort.php
@@ -13,8 +13,26 @@ c. median of the array
 d. random element of the array
 ]
 
-1. Pick a pivot & place it in its correct place in the sorted array
+Step 1. Pick a pivot & place it in its correct place in the sorted array
  
-2. Smaller on the left and larger on the right
+Step 2. Smaller on the left and larger on the right
+
+*/
+
+/*
+# Pseudo code
+index [0, 1, 2, 3, 4, 5, 6, 7]
+array [4, 6, 2, 5, 7, 9, 1, 3]
+low = 0 [i], high = 7 [j]
+pivot = array[low]
+
+if(pivot > array[i]) stop 1
+then check 
+if(pivot > array[j]) stop j
+and swap j with i
+repeat until i and j are not crossed
+when pivot is placed in the correct order let pivot as a partition index
+
+
 
 */
