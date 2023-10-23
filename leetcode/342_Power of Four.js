b1add747e03f46da6877d004056ce6a4379b9114
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2023-10-23 15:47:32 +0530

    Update 342_Power of Four.js
    increase runtime

--- leetcode/342_Power of Four.js
+++ leetcode/342_Power of Four.js
@@ -1,16 +1,15 @@
 function isPowerOfFour(n: number): boolean {
-    return n > 0 && (n & (n - 1)) === 0 && (n & 0x55555555) === n;
+    const recur = (n: number): boolean => {
+        if (n === 1){
+            return true
+        }
+
+        if (n < 4 || n % 4 !== 0){
+            return false
+        }
+
+        return recur(n / 4)
+    }
+
+    return recur(n); 
 };
-
-
-/*
-0x55555555 is a hexadecimal representation of a 32-bit binary number, where each digit represents 4 binary bits. In binary, it looks like this:
-
-0101 0101 0101 0101 0101 0101 0101 0101
-This binary number has a unique property that makes it useful for certain bitwise operations. 
-It has set bits (1s) at odd positions, starting from the least significant bit (LSB). The set bits are at positions 1, 9, 17, 25, and so on. 
-This property is valuable when you want to check whether a specific bit in an integer is set.
-
-In the context of the isPowerOfFour function, n & 0x55555555 is used to check whether the set bit in n (if it's a power of two) is at an odd position. 
-If this condition is true, it indicates that the set bit is at an odd position, which helps determine if n is a power of four.
-*/
