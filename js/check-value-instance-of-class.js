4f8bb6ae5bf528c813a6d8f4ccdbac93d1bd3c45
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2024-12-19 06:41:22 +0530

    2618. Check if Object Instance of Class

---
+++ js/check-value-instance-of-class.js
@@ -0,0 +1,28 @@
+// 2618. Check if Object Instance of Class
+// 5 Number = true
+// '5' Number = false
+// new Date() Date = true
+
+function checkInstanceOf(value, targetClass){
+    if(typeof targetClass !== "function"){
+        return false;
+    }
+
+    // handling the primitive values
+    if(value !== null && typeof value !== 'object' && typeof value !== 'function'){
+        return Object.getPrototypeOf(Object(value)) === targetClass.prototype;
+    }
+
+    while(value !== null){
+        if(Object.getPrototypeOf(value) === targetClass.prototype){
+            return true;
+        }
+        value = Object.getPrototypeOf(value);
+    }
+
+    return false;
+}
+
+console.log(checkInstanceOf(5, Number))
+console.log(checkInstanceOf('5', Number))
+console.log(checkInstanceOf(new Date(), Date))
