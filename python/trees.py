0618668fea6248e34bf9b334c4e5d3ad023939e8
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2025-05-12 15:44:45 +0530

    tree dsa problems
    Maximum Depth of Binary Tree:
    Return the maximum depth of the tree.
    
    Count Leaf Nodes:
    Count the number of leaf nodes in a binary tree.
    
    Mirror of a Binary Tree:
    Convert the binary tree into its mirror image.
    
    Symmetric Tree:
    Check whether a binary tree is symmetric.
    
    Sum of All Nodes:
    Calculate the sum of all node values in a binary tree.
    
    Sum of Left Leaves:
    Find the sum of all left leaf nodes.

--- python/trees.py
+++ python/trees.py
@@ -4,6 +4,7 @@ class Node:
         self.left = None
         self.right = None
 
+# The maximum depth (or height) of a binary tree is the number of nodes along the longest path from the root node down to the furthest leaf node.
 def findHeight(root):
     if root is None:
         return -1
@@ -57,11 +58,71 @@ def check_balance(root):
     return is_balanced
 
 
-# root = Node(1)
-# root.left = Node(2)
+# A leaf node in a binary tree is a node that does not have any children (both left and right children are None).
+def count_leaf_node(root):
+    if not root:
+        return 0
+    # if the current node is a leaf node
+    if not root.left and not root.right:
+        return 1
+    
+    left_count = count_leaf_node(root.left)
+    right_count = count_leaf_node(root.right)
+
+    return left_count + right_count
+    
+# A mirror image of a binary tree is a tree where left and right children of every node are swapped. In other words, the mirror of a binary tree is obtained by flipping the tree around its root.
+def mirror_tree(root):
+    if not root:
+        return None
+    
+    root.left, root.right = root.right, root.left
+
+    mirror_tree(root.left)
+    mirror_tree(root.right)
+
+    return root
+
+
+# A binary tree is symmetric if it is a mirror image of itself. In other words, the left subtree is a mirror reflection of the right subtree.
+def is_mirror(left, right):
+    
+    # Both nodes are None: symmetric
+    if not left and not right:
+        return True
+
+    # One of them is None: not symmetric
+    if not left or not right:
+        return False
+    
+    # Check values and recurse for left and right children
+    return (left.value == right.value and is_mirror(left.left, right.right) and is_mirror(left.right, right.left) )
+
+def is_symmetric(root):
+    if not root:
+        return True
+    
+    return is_mirror(root.left, root.right)
+
+def sum_of_left_leaves(root):
+    if not root:
+        return 0
+    
+    total_sum = 0
+    # Check if the left child is a leaf
+    if root.left and not root.left.left and not root.left.right:
+        total_sum += root.left.key
+
+    # Recursively find the sum of left leaves in left and right subtrees
+    total_sum += sum_of_left_leaves(root.left)
+    total_sum += sum_of_left_leaves(root.right)
+
+    return total_sum
+
+
 
 root = None
-keys = [5,10,2,30]
+keys = [5, 10, 2, 30, 15, 3, 9, 1, 20, 8, 6, 4, 7]
 for key in keys:
     root = insert(root, key)
 
@@ -70,3 +131,5 @@ print("In Order:", inorder(root))
 print("Height:", findHeight(root))
 
 print("is Balance:", check_balance(root))
+
+print("Left leaf sum:", sum_of_left_leaves(root))
